70941504d5f7532e37cbdefd17024ff32ef783ba
W_light <pengcheng.bai@outlook.com>
2024-08-05 19:28:23 +0800

    Update 3.clone.sh

--- preset-lienol-openwrt/3.clone.sh
+++ preset-lienol-openwrt/3.clone.sh
@@ -27,3 +27,5 @@ git clone --depth 1 https://github.com/vernesong/OpenClash.git && mv OpenClash/l
 
 # download more codes
 git clone --depth 1 https://github.com/sbwml/openwrt_helloworld.git temp && mv ./temp/chinadns-ng ./ && rm -rf temp
+git clone --depth 1 https://github.com/immortalwrt/luci.git temp && mv ./temp/applications/luci-app-appfilter ./ && rm -rf temp
+git clone --depth 1 https://github.com/destan19/OpenAppFilter.git
